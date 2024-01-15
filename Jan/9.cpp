3a090c527da874060054b14f5cd08e56f3df5e66
Piyush Kumar <pk421780@gmail.com>
2024-01-15 18:29:47 +0530

    kmp alorithm

--- Jan/9.cpp
+++ Jan/9.cpp
@@ -4,35 +4,76 @@ using namespace std;
 class Solution
 {
 public:
+    void findLps(string pattern, vector<int> &lps)
+    {
+        int m = pattern.length();
+        int len = 0;
+
+        lps[0] = 0;
+
+        int i = 1;
+        while (i < m)
+        {
+            if (pattern[i] == pattern[len])
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else
+            {
+                if (len != 0)
+                {
+                    len = lps[len - 1];
+                }
+                else
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
     vector<int> search(string pat, string txt)
     {
-        // code here
+        int n = txt.length();
+        int m = pat.length();
+        vector<int> result;
 
-        int n = txt.size();
-        int k = pat.size();
+        vector<int> lps(m, 0);
+        findLps(pat, lps);
 
-        if (k > n)
+        int i = 0;
+        int j = 0;
+
+        while (i < n)
         {
-            return {-1};
-        }
-
-        vector<int> v;
-
-        for (int i = 0; i < n - k; i++)
-        {
-
-            if (pat == txt.substr(i, k))
+            if (pat[j] == txt[i])
             {
-                v.push_back(1);
+                i++;
+                j++;
+            }
+
+            if (j == m)
+            {
+                result.push_back(i - j + 1);
+                j = lps[j - 1];
+            }
+            else if (i < n && pat[j] != txt[i])
+            {
+                if (j != 0)
+                {
+                    j = lps[j - 1];
+                }
+                else
+                {
+                    i++;
+                }
             }
         }
 
-        if (v.size() == 0)
-        {
-            return {-1};
-        }
-
-        return v;
+        return result;
     }
 };
 
@@ -40,3 +81,5 @@ int main()
 {
     return 0;
 }
+
+// link : https://www.geeksforgeeks.org/problems/search-pattern0205/1
