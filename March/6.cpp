6e524cf32d3e2ec865ccefed5cff4585b2814029
Piyush Kumar <pk421780@gmail.com>
2024-03-06 16:24:44 +0530

    Rabin Karp Algorithm

--- March/6.cpp
+++ March/6.cpp
@@ -4,6 +4,8 @@ using namespace std;
 class Solution
 {
 public:
+    // TC : O(n*m)
+    /*
     vector<int> search(string pattern, string text)
     {
         // code here.
@@ -17,6 +19,56 @@ public:
             }
         }
 
+        return v;
+    }
+    */
+
+   //TC : O(n+m)
+    void rabinKarpAlgo(string p, string t, vector<int> &v)
+    {
+        int n = t.size(), m = p.size();
+        int mod = INT_MAX, base = 256;
+
+        int hash = 1;
+        for (int i = 0; i < m - 1; i++)
+        {
+            hash = (hash * base) % mod;
+        }
+
+        int pHash = 0, tHash = 0;
+        for (int i = 0; i < m; i++)
+        {
+            pHash = (base * pHash + p[i]) % mod;
+            tHash = (base * tHash + t[i]) % mod;
+        }
+
+        for (int i = 0; i <= n - m; i++)
+        {
+            if (pHash == tHash)
+            {
+                if (t.substr(i, m) == p)
+                {
+                    v.push_back(i + 1);
+                }
+            }
+            else
+            {
+                if (i < n - m)
+                {
+                    tHash = (base * (tHash - t[i] * hash) + t[i + m]) % mod;
+                }
+                if (tHash < 0)
+                {
+                    tHash = tHash + mod;
+                }
+            }
+        }
+    }
+
+    vector<int> search(string pattern, string text)
+    {
+        vector<int> v;
+        rabinKarpAlgo(pattern, text, v);
         return v;
     }
 };
