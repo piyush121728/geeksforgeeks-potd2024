c1c4ed201c01643c6924687efa513fb6ce6d75b5
Piyush Kumar <pk421780@gmail.com>
2024-04-06 18:20:08 +0530

    6 april potd is completed.

---
+++ April/6.cpp
@@ -0,0 +1,11 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long countWays(int n)
+{
+    // your code here
+    return (n / 2) + 1;
+}
+
+// link : https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1
