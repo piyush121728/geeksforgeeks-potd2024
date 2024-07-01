506168220907d3d8dd2bd2d56f1fc5bfcee930f3
Piyush Kumar <pk421780@gmail.com>
2024-07-01 10:45:00 +0530

    1 july potd is completed.

---
+++ July/1.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+struct TreeNode
+{
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Function to make binary tree from linked list.
+TreeNode *constructTree(int i, int n, vector<int> &v)
+{
+    if (i >= n)
+    {
+        return NULL;
+    }
+
+    TreeNode *root = new TreeNode(v[i]);
+    root->left = constructTree(2 * i + 1, n, v);
+    root->right = constructTree(2 * i + 2, n, v);
+    return root;
+}
+void convert(Node *head, TreeNode *&root)
+{
+    // Your code here
+    vector<int> v;
+    while (head)
+    {
+        v.push_back(head->data);
+        head = head->next;
+    }
+
+    int n = v.size();
+    root = constructTree(0, n, v);
+}
+
+// link :https://www.geeksforgeeks.org/problems/make-binary-tree/1
